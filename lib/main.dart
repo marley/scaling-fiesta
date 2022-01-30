7d79a3e838a2dc7150259151f27d0e32f926e554
Marley Alford <marleycodes@gmail.com>
2022-01-29 20:43:39 -0500

    Added RandomWord widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,17 +14,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: Center(
-            child: Text(wordPair.asPascalCase)
+        body: const Center(
+            child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState(); // underscore enforces privacy, which is best practice
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
